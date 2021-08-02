64f1cfafb72ba4df5418b7c2ae951c709697ae50
John Stritch <john.stritch@icloud.com>
2021-08-02 11:34:00 -0500

    initial commit

---
+++ spec/git-tabular-diff-spec.js
@@ -0,0 +1,73 @@
+'use babel';
+
+import GitTabularDiff from '../lib/git-tabular-diff';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('GitTabularDiff', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('git-tabular-diff');
+  });
+
+  describe('when the git-tabular-diff:toggle event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.git-tabular-diff')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'git-tabular-diff:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.git-tabular-diff')).toExist();
+
+        let gitTabularDiffElement = workspaceElement.querySelector('.git-tabular-diff');
+        expect(gitTabularDiffElement).toExist();
+
+        let gitTabularDiffPanel = atom.workspace.panelForItem(gitTabularDiffElement);
+        expect(gitTabularDiffPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'git-tabular-diff:toggle');
+        expect(gitTabularDiffPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.git-tabular-diff')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'git-tabular-diff:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let gitTabularDiffElement = workspaceElement.querySelector('.git-tabular-diff');
+        expect(gitTabularDiffElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'git-tabular-diff:toggle');
+        expect(gitTabularDiffElement).not.toBeVisible();
+      });
+    });
+  });
+});
---
+++ spec/git-tabular-diff-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GitTabularDiffView from '../lib/git-tabular-diff-view';
+
+describe('GitTabularDiffView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
